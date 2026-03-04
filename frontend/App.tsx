37e1dd04b66e1b27435f682e422453e1333ccf34
Emmanuel Morales <emorales14@cuc.edu.co>
2026-03-03 22:16:24 -0500

    Initial commit: proyecto limpio y organizado

---
+++ frontend/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>Clean Start</h1>
+            <p>Your project is ready for frontend development.</p>
+        </>
+    )
+}
+
+export default App
